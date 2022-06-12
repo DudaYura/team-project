824a8a1923f8c182104575b8a8230638ea413935
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-06-12 17:26:06 +0300

    добавлен скрипт открытия модального франшизы

--- src/modal-franchise.js
+++ src/modal-franchise.js
@@ -1,1 +1,16 @@
+(() => {
+  const openFranchiseModalBtn = document.querySelector('.cnt-btn__fr');
+  const closeFranchiseModalBtn = document.querySelector('.modal-close-btn-fr');
+  const franchiseModal = document.querySelector('[data-modal-fr]');
 
+  const openFranchiseModal = () => {
+    franchiseModal.classList.remove('is-hidden');
+  };
+
+  const closeFranchiseModal = () => {
+    franchiseModal.classList.add('is-hidden');
+  };
+
+  openFranchiseModalBtn.addEventListener('click', openFranchiseModal);
+  closeFranchiseModalBtn.addEventListener('click', closeFranchiseModal);
+})();
