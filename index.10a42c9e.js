cc60692c6a7db7a369bba253a0da54551a5c027e
DudaYura <99014047+DudaYura@users.noreply.github.com>
2022-06-13 19:23:05 +0000

    Deploying to gh-pages from @ DudaYura/team-project@948ea4a74790ffec7bd2028ea29bfe5f5fe7aa44 🚀

---
+++ index.10a42c9e.js
@@ -0,0 +1,2 @@
+(()=>{const t={openAboutBtn:document.querySelector("[data-about-modal-open]"),closeAboutBtn:document.querySelector("[data-about-modal-close]"),aboutmodal:document.querySelector("[data-about-modal]")};function o(){t.aboutmodal.classList.toggle("is-hidden")}t.openAboutBtn.addEventListener("click",o),t.closeAboutBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.10a42c9e.js.map
