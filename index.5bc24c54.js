28bd3896a2a7be14d469d886bb57631f814a1688
DudaYura <99014047+DudaYura@users.noreply.github.com>
2022-06-13 19:06:56 +0000

    Deploying to gh-pages from @ DudaYura/team-project@ac599799128e2e8b00eba701c29b4b8b41c5d117 🚀

--- index.5bc24c54.js
+++ index.5bc24c54.js
@@ -1,2 +1,2 @@
 !function(){var o={openAboutBtn:document.querySelector("[about-modal-open]"),closeAboutBtn:document.querySelector("[about-modal-close]"),aboutmodal:document.querySelector("[about-modal]")};function t(){o.aboutmodal.classList.toggle("is-hidden")}o.openAboutBtn.addEventListener("click",t),o.closeAboutBtn.addEventListener("click",t)}();
-//# sourceMappingURL=index.c98d7327.js.map
+//# sourceMappingURL=index.5bc24c54.js.map
---
+++ index.9f617a2f.js
@@ -0,0 +1,2 @@
+(()=>{const o={openAboutBtn:document.querySelector("[about-modal-open]"),closeAboutBtn:document.querySelector("[about-modal-close]"),aboutmodal:document.querySelector("[about-modal]")};function t(){o.aboutmodal.classList.toggle("is-hidden")}o.openAboutBtn.addEventListener("click",t),o.closeAboutBtn.addEventListener("click",t)})();
+//# sourceMappingURL=index.9f617a2f.js.map
